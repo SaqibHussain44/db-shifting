28506b97dd940b631fef92f9593f39cc9016a304
saqibhussain44 <saqibhussain990@gmail.com>
2020-05-18 05:17:50 +0500

    change port

--- app.js
+++ app.js
@@ -229,7 +229,7 @@ app.get("/test-reading", function(req,res) {
 	})
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(8080, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
